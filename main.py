8be32976ef6232a279b08b51896520217578de25
Your Name <you@example.com dorothy.corley@stonybrook.edy>
2023-05-13 02:05:44 +0000

    change

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 import pandas as pd
+import uvicorn
 
-df = pd.read_csv('./data/diagnoses2019.csv')
+df = pd.read_csv('./data/diagnoses.csv')
 
 app = FastAPI()
 
 @app.get('/')
 def home():
-    return 'this is a API service for MN ICD code details'
+    return {'this is an API service for MN code details'}
 
 @app.get('/preview')
 async def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return result
+
 
 @app.get('/icd/{value}') 
 async def icdcode(value):
@@ -24,6 +26,7 @@ async def icdcode(value):
     else:
         return filtered.to_json(orient="records")
 
+
 @app.get('/icd/{value}/sex/{value2}') 
 async def icdcode2(value, value2):
     filtered = df[df['principal_diagnosis_code'] == value]
@@ -32,3 +35,4 @@ async def icdcode2(value, value2):
         return 'There is nothing here'
     else:
         return filtered2.to_json(orient="records")
+    
