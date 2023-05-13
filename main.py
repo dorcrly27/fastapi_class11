f52d4bbb70e02d27be37167a859893dacae8883e
Your Name <you@example.com dorothy.corley@stonybrook.edy>
2023-05-13 02:19:50 +0000

    change

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ df = pd.read_csv('./data/diagnoses.csv')
 
 app = FastAPI()
 
-@app.get('/')
+app.get('/')
 def home():
     return {'this is an API service for MN code details'}
 
-@app.get('/preview')
+app.get('/preview')
 async def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return result
 
 
-@app.get('/icd/{value}') 
+app.get('/icd/{value}') 
 async def icdcode(value):
     print('value: ', value)
     filtered = df[df['principal_diagnosis_code'] == value]
@@ -27,7 +27,7 @@ async def icdcode(value):
         return filtered.to_json(orient="records")
 
 
-@app.get('/icd/{value}/sex/{value2}') 
+app.get('/icd/{value}/sex/{value2}') 
 async def icdcode2(value, value2):
     filtered = df[df['principal_diagnosis_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
